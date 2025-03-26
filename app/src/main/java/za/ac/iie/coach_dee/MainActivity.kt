df4633bdfb41274a49d80a37b6cc29ab39c30f99
lab_services_student <ST10474638@vcconnect.edu.za>
2025-03-26 11:24:51 +0200

    Coach Dee is cool

--- app/src/main/java/za/ac/iie/coach_dee/MainActivity.kt
+++ app/src/main/java/za/ac/iie/coach_dee/MainActivity.kt
@@ -47,6 +47,51 @@ fun GreetingPreview() {
 }
 
 fun DoingWhatIWantToDo(){
+    /*
+    In this program it will be demonstrated on how to prompt an individual on entering their
+    information. To check if they qualify for pension and if they also are allowed to drive.
+     */
+
+    //Prompt the individual to enter their first name.//
     println("Enter your first name")
-    val Name_01 =
+    val Name_01 = readLine()
+
+    //Prompt the individual to enter their age.//
+    println("Enter your age")
+    val Age = readLine()?.toIntOrNull() ?:0
+
+    /*
+    Make a decision statement to determine whether the individual qualifies for pension and/or is
+    allowed to drive
+     */
+
+    //Decision statement to see if the individual qualifies for pension.//
+    if(Age>=50) {
+        println("Yes: You do qualify for pension.")
+    }else{
+        println("No: You do not qualify for pension.")
+    }
+   //Statement of decision to check if the individual is allowed to drive or not.//
+    if(Age>=19){
+        println("Yes: You are allowed to drive.")
+    }else{
+        println("No: You are not allowed to drive")
+    }
+}
+fun Quetionaires(){
+    //Question 1 is here to evaluate the student' on-campus experience.//
+    println("Question 1" +
+            "Are you happy with the experience at the IIE' Varsity College and what course and the year are you completing?")
+    val Answer_001 = readln()
+
+    //Question 2 is here to determine whether the student will continue with their here at the IIE' Varsity College.//
+    println("Question 2" +
+            "Inclusive of your experience here on campus will you be furthering your studies here at the IIE' Varsity College?")
+    val Answer_002 = readln()
+
+    //
+
+}
+fun main(){
+    println(DoingWhatIWantToDo())
 }
